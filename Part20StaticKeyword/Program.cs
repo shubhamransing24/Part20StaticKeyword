187a3e29fdce3e849274f5e96fdff9c1504c71d9
priyasonar1207 <83229578+priyasonar1207@users.noreply.github.com>
2021-10-06 10:55:47 +0530

    Add project files.

---
+++ Part20StaticKeyword/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Part20StaticKeyword
+{
+    class Circle
+    {
+        float _pi;
+        int _radius;
+        public Circle(int radius)
+        {
+            this._pi = 3.141f;
+            this._radius = radius;
+
+        }
+        public void AreaOfCircle()
+        {
+            Console.WriteLine("You Enterd Radius :{0}", this._radius);
+            Console.WriteLine("Area of Circle is ={0}", this._pi * this._radius * this._radius);
+
+        }
+    }
+ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("To Calculate Area Of Circle ");
+            Console.WriteLine("Plese Enter the Readius:");
+            int radius = int.Parse(Console.ReadLine());
+            Circle c = new Circle(radius);
+            c.AreaOfCircle();
+        }
+    }
+
+}
